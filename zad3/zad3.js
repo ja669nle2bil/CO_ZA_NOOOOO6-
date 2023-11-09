1733dcfc70269bbf6fb325ecb987b53e99b6ead2
ja669nle2bil <147039919+ja669nle2bil@users.noreply.github.com>
2023-11-09 10:50:06 +0100

    STRUGGLING with js 😪

--- zad3/zad3.js
+++ zad3/zad3.js
@@ -3,5 +3,19 @@ function range(a,b){
 	for(let i=a; i<=b; i++) {
 			arr.push(i);
 	}
+	console.log(arr);
 	return arr;
 }
+/* console.log(range(2,4)); */
+range(2,7);
+
+
+/* const fs=require('fs');
+fs.readFile('path/to/file.js', 'utf8', (err, data) => {
+		if(err) {
+			console.error(err);
+			return
+		}
+		console.log(data);
+});
+*/
